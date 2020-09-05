fd9b251c7ef710b377132720a41d7698e0c14f5b
Ross Libby <ross@frvctl.com>
2020-09-04 21:35:56 -0500

    initial commit

---
+++ src/Input/Text.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import 'styles/input.scss'
+
+class Text extends React.Component {
+  constructor() {
+    super()
+    this._change = this._change.bind(this)
+    this.state = {text: ''}
+  }
+
+  _change(e) {
+    this.setState({text: e.target.value})
+  }
+
+  render() {
+    return (
+      <div className="input">
+        <label className="input-label">
+          <input type="text" className={`input-text${this.state.text.length > 0 ? ' input-text--filled' : ''}`} onChange={this._change} />
+          <span className="input-label__text">Address</span>
+        </label>
+      </div>
+    )
+  }
+}
+
+export default Text
